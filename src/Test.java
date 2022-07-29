307d1133da8efef32332dd7d92a9c645e7fa0159
Sachin Pawar <sachinpawar80@gmail.com>
2022-07-29 08:28:31 +0530

    new test

--- src/Test.java
+++ src/Test.java
@@ -2,6 +2,12 @@
 public class Test {
 
 	public static void main(String[] args) {
+		System.out.println();
+		System.out.println();
+		int a=10;
+		int b=20;
+		System.out.println(a+b);
+		System.out.println(a);
 		
 	}
 }
